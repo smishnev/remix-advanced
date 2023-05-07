108a3cd0695d6049eb9d76ae115aa02af63f83af
Sergey Mishnev <s.mishnev@forsit.de>
2023-05-07 16:42:18 +0300

    feat: editing data expense without saving

--- app/components/expenses/ExpenseForm.tsx
+++ app/components/expenses/ExpenseForm.tsx
@@ -1,11 +1,42 @@
-import { Form, Link, useActionData, useNavigation } from '@remix-run/react';
+import {
+  Form,
+  Link,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from '@remix-run/react';
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
   const validationErrors = useActionData();
+  const expenseData = useLoaderData();
   const navigation = useNavigation();
 
+  const defaultValues = expenseData
+    ? {
+        title: expenseData.title,
+        amount: expenseData.amount,
+        date: expenseData.date,
+      }
+    : {
+        title: '',
+        amount: '',
+        date: '',
+      };
+
   const isSubmitting = navigation.state !== 'idle';
+
+  // const submit = useSubmit();
+
+  // function submitHandler(event) {
+  //   event.preventDefault();
+  //   // perform your own validation
+  //   // ...
+  //   submit(event.target, {
+  //     // action: '/expenses/add',
+  //     method: 'post',
+  //   });
+  // }
 
   return (
     <Form
@@ -16,7 +47,14 @@ function ExpenseForm() {
     >
       <p>
         <label htmlFor="title">Expense Title</label>
-        <input type="text" id="title" name="title" required maxLength={30} />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          maxLength={30}
+          defaultValue={defaultValues.title}
+        />
       </p>
 
       <div className="form-row">
@@ -29,11 +67,21 @@ function ExpenseForm() {
             min="0"
             step="0.01"
             required
+            defaultValue={defaultValues.amount}
           />
         </p>
         <p>
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" max={today} required />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            max={today}
+            required
+            defaultValue={
+              defaultValues.date ? defaultValues.date.slice(0, 10) : ''
+            }
+          />
         </p>
       </div>
       {validationErrors && (
--- app/routes/__app/expenses/$id.tsx
+++ app/routes/__app/expenses/$id.tsx
@@ -1,16 +1,27 @@
+// /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
+
 import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
+import { getExpense } from '~/data/expenses.server';
 
-export default function AddExpensesPage() {
+export default function UpdateExpensesPage() {
   const navigate = useNavigate();
 
-	function closeHandler() {
-		navigate('..')
-	}
+  function closeHandler() {
+    // navigate programmatically
+    navigate('..');
+  }
+
   return (
     <Modal onClose={closeHandler}>
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function loader({params}: any) {
+  const expenseId = params.id;
+  const expense = await getExpense(expenseId)
+  return expense;
 }
