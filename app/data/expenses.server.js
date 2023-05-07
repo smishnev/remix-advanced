5bf6fdad9100cf28c15d87d2af47ef8311d2b609
Sergey Mishnev <s.mishnev@forsit.de>
2023-05-07 16:26:28 +0300

    feat: fetch data from db

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -1,4 +1,3 @@
-
 import { prisma } from './database.server';
 
 export async function addExpense(expenseData) {
@@ -6,10 +5,32 @@ export async function addExpense(expenseData) {
     return await prisma.expense.create({
       data: {
         title: expenseData.title,
-        amount: +expenseData.amount,   
+        amount: +expenseData.amount,
         date: new Date(expenseData.date),
       },
     });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getExpenses() {
+  try {
+    const expenses = await prisma.expense.findMany({
+      orderBy: { date: 'desc' },
+    });
+    return expenses;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getExpense(id) {
+  try {
+    const expense = await prisma.expense.findFirst({ where: { id } });
+    return expense;
   } catch (error) {
     console.log(error);
     throw error;
