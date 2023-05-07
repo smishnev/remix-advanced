415f3955f94497cc0566e355f0dcdfe208dfef95
Sergey Mishnev <s.mishnev@forsit.de>
2023-05-07 12:50:56 +0300

    feat: add splat routes

--- app/routes/expenses.raw.tsx
+++ app/routes/expenses.raw.tsx
@@ -1,3 +1,20 @@
-export default function ExpensesRawPage() {
-	return <h1>Expenses Raw Page</h1>;
+// /expenses/raw
+
+const DUMMY_EXPENSES = [
+	{
+	  id: 'e1',
+	  title: 'First Expense',
+	  amount: 12.99,
+	  date: new Date().toISOString(),
+	},
+	{
+	  id: 'e2',
+	  title: 'Second Expense',
+	  amount: 16.99,
+	  date: new Date().toISOString(),
+	},
+  ];
+  
+  export function loader() {
+	return DUMMY_EXPENSES;
   }
