d55264eb87febe9c624878401c8e5f4a05ba46c3
Sergey Mishnev <s.mishnev@forsit.de>
2023-05-18 10:23:12 +0300

    feat: updating expense

--- app/routes/__app/expenses/$id.tsx
+++ app/routes/__app/expenses/$id.tsx
@@ -1,9 +1,12 @@
 // /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
 
+import { redirect } from '@remix-run/node';
 import { useNavigate } from '@remix-run/react';
 
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
+import { updateExpense } from '~/data/expenses.server';
+import { validateExpenseInput } from '~/data/validation.server';
 // import { getExpense } from '~/data/expenses.server';
 
 export default function UpdateExpensesPage() {
@@ -22,7 +25,23 @@ export default function UpdateExpensesPage() {
 }
 
 // export async function loader({params}) {
+//   console.log('EXPENSE ID LOADER');
 //   const expenseId = params.id;
 //   const expense = await getExpense(expenseId)
 //   return expense;
 // }
+
+export async function action({params, request}: {params:any, request:any }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect('/expenses'); 
+}
