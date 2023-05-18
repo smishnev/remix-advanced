9c44d52562ca8b076d7ca04c91ce5588cc697fde
Sergey Mishnev <s.mishnev@forsit.de>
2023-05-18 15:46:55 +0300

    feat: logout functionality implemented

--- app/data/auth.server.js
+++ app/data/auth.server.js
@@ -26,6 +26,34 @@ async function createUserSession(userId, redirectPath) {
   });
 }
 
+export async function getUserFromSession(request) {
+	const session = await sessionStorage.getSession(
+		request.headers.get('Cookie')
+	);
+
+	const userId = session.get('userId');
+
+	if (!userId) {
+		return null;
+	}
+
+	return userId;
+}
+
+
+export async function destroyUserSession(request) {
+	const session = await sessionStorage.getSession(
+		request.headers.get('Cookie')
+	);
+
+	return redirect('/', {
+		headers: {
+		'Set-Cookie': await sessionStorage.destroySession(session),
+		},
+	});
+}
+
+
 export async function signup({ email, password }) {
   const existingUser = await prisma.user.findFirst({ where: { email } });
 
--- app/routes/__marketing.tsx
+++ app/routes/__marketing.tsx
@@ -1,15 +1,20 @@
 import { Outlet } from '@remix-run/react';
-import MainHeader from '~/components/navigation/MainHeader';
 
+import MainHeader from '~/components/navigation/MainHeader';
+import { getUserFromSession } from '~/data/auth.server';
 import marketingStyles from '~/styles/marketing.css';
 
 export default function MarketingLayout() {
   return (
-	<>
-	<MainHeader />
-	<Outlet />
-	</>
-  )
+    <>
+      <MainHeader />
+      <Outlet />;
+    </>
+  );
+}
+
+export function loader({ request }: any) {
+  return getUserFromSession(request);
 }
 
 export function links() {
